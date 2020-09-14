b5ec2998ea49a38427c5ea95462843ab92db5501
cb <ranggawahyuds@gmail.com>
2020-09-14 10:48:13 +0700

    Upload di Github

---
+++ akun.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+
+clear
+echo "\033[1;32m        START "
+sleep 1s
+
+ulang="y"
+
+while [ $ulang = "y" ]
+do
+
+python clickbotv3.1.py +6289512891329 ltc
+python clickbotv3.1.py +6289602987793 ltc
+python clickbotv3.1.py +6289694427574 ltc
+python clickbotv3.1.py +6289693721996 ltc
+python clickbotv3.1.py +6289509200181 ltc
+python clickbotv3.1.py +6281256693444 ltc
+python clickbotv3.1.py +13175423037 ltc
+python clickbotv3.1.py +13158137537 ltc
+python clickbotv3.1.py +6289693721996 ltc
+
+x=10000
+while [ $x -gt 0 ]
+do
+sleep 1s
+clear
+echo " \033[1;32m Mulai ulang sisa Waktu anda $x Detik"
+x=$(( $x - 1 ))
+done
+
+
+
+done
